9a6b1734dde4c7f9e46156e622bffb1b6ae409a1
Ángel Juárez <angelnjuarez@gmail.com>
2023-03-30 18:48:41 -0300

    Resto de controllers

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Get('filter') //No dinámico va primero
+  getFilter() {
+    return 'I am filter';
+  }
+
+  @Get(':productId') //los slash al final son opcionales, en otros frameworks no.
+  getProduct(@Param('productId') productId: string) {
+    //Recibimos el objeto, sin recorrer params
+    return `Product ${productId}`;
+  }
+
+  @Get()
+  getProducts(
+    @Query('limit') limit = 100, //Infiere number
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products: limit => ${limit} offset => ${offset} brand => ${brand}`;
+  }
+}
